eacd7c4e2279a325ced05a66f008463b111c1d3f
Thomas Overgaard <68824543+Schwencke@users.noreply.github.com>
2021-05-17 18:58:11 +0200

    Admin edit now shows correctly

--- src/main/java/web/commands/CommandUpdateMeasurements.java
+++ src/main/java/web/commands/CommandUpdateMeasurements.java
@@ -1,20 +1,23 @@
 package web.commands;
 
 
+import business.entities.Order;
 import business.exceptions.UserException;
 import business.services.OrderFacade;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 
 public class CommandUpdateMeasurements extends CommandProtectedPage {
     protected OrderFacade orderFacade;
+    Order order;
     int carportWidth;
     int carportLength;
     int shedLength;
     int shedWidth;
     int orderId;
-    int statusId;
 
     public CommandUpdateMeasurements(String pageToShow, String role) {
         super(pageToShow, role);
@@ -23,7 +26,7 @@ public class CommandUpdateMeasurements extends CommandProtectedPage {
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
-
+        HttpSession session = request.getSession();
         orderId = Integer.parseInt(request.getParameter("orderid"));
 
         carportLength = Integer.parseInt(request.getParameter("carportLength"));
@@ -35,6 +38,8 @@ public class CommandUpdateMeasurements extends CommandProtectedPage {
 
         orderFacade.updateShedMeasurementsById(orderId, shedLength, shedWidth);
 
+        order = orderFacade.getOrderById(orderId);
+        session.setAttribute("order", order);
 
         return pageToShow;
     }
