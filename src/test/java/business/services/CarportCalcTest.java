519ede660337553f3c44e7fc2a4ea0813d304cac
PeterDaniel3D <75070076+PeterDaniel3D@users.noreply.github.com>
2021-05-27 15:44:32 +0200

    Updated tests

--- src/test/java/business/services/CarportCalcTest.java
+++ src/test/java/business/services/CarportCalcTest.java
@@ -32,36 +32,69 @@ class CarportCalcTest {
 
     @Test
     void calcPost() throws UserException {
+        Material material = new Material();
         List<Material> result = new ArrayList<>();
-        System.out.println(result.add(new Material()));
+        result.add(carportCalc.newItem(6, 1601, material));
         assertEquals(result, carportCalc.calcPost(6000, 7800));
     }
 
     @Test
     void calcBeam() throws UserException {
+        Material material = new Material();
         List<Material> result = new ArrayList<>();
-        System.out.println(result.add(new Material()));
+        result.add(carportCalc.newItem(2, 1514, material));
+        result.add(carportCalc.newItem(2, 1506, material));
         assertEquals(result, carportCalc.calcBeam(6000, 7800));
     }
 
     @Test
     void calcRafter() throws UserException {
-        List<Material> result = new ArrayList<>();
-        System.out.println(result.add(new Material()));
-        assertEquals(result, carportCalc.calcRafter(6000, 7800));
+        assertEquals(new Material(), carportCalc.calcRafter(6000, 7800));
     }
 
     @Test
     void calcRoofing() throws UserException {
-        List<Material> result = new ArrayList<>();
-        System.out.println(result.add(new Material()));
-        assertEquals(result, carportCalc.calcRoofing(6000, 7800));
+        assertEquals(new Material(), carportCalc.calcRoofing(6000, 7800));
     }
 
     @Test
     void sternUnderFrontAndBack() throws UserException {
-        List<Material> result = new ArrayList<>();
-        System.out.println(result.add(new Material()));
-        assertEquals(result, carportCalc.calcSternUnderFrontAndBack(6000));
+        assertEquals(new Material(), carportCalc.calcSternUnderFrontAndBack(6000));
+    }
+
+    @Test
+    void newItem() {
+    }
+
+    @Test
+    void calcStern() {
+    }
+
+    @Test
+    void calcSternUnderFrontAndBack() {
+    }
+
+    @Test
+    void calcSternUnderSides() {
+    }
+
+    @Test
+    void calcSternOverFront() {
+    }
+
+    @Test
+    void calcSternOverSides() {
+    }
+
+    @Test
+    void calcSternWaterFront() {
+    }
+
+    @Test
+    void calcSternWaterSides() {
+    }
+
+    @Test
+    void testCalcRoofing() {
     }
 }
